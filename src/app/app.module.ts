e7ed8faa202fe78dfdff39e7af878ca26f88e93b
Bruhopeless <34315908+Bruhopeless@users.noreply.github.com>
2024-01-03 20:53:06 +0200

    Add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  form!: FormGroup;
 
+  ngOnInit(): void {
+    this.form = new FormGroup ({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      pass: new FormControl('', Validators.required),
+      repPass: new FormControl ('', Validators.required)},
+      { validators: this.passwordSame } as any
+    );
+  }
+
+  passwordSame (formGroup: FormGroup):any {
+    const password = formGroup.get('pass')?.value;
+    const confirmPassword = formGroup.get('repPass')?.value;
+
+    if (password === confirmPassword || (!password && !confirmPassword)) {
+      return null;
+    }
+    return { 'passwordMismatch': true };
+  }
+
+  onSubmit() {
+    
+  }
 }
