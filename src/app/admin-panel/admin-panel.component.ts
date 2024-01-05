bf130a04f12ef019866c6e956b3b4afdb9466a5e
Bruhopeless <34315908+Bruhopeless@users.noreply.github.com>
2024-01-05 19:43:47 +0200

    Done admin panel products

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,10 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductModel, ProductsService } from '../service/products.service';
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css'
 })
-export class AdminPanelComponent {
+export class AdminPanelComponent implements OnInit {
 
+  constructor(private productDb: ProductsService) {}
+
+  products: any[] = [];
+
+  nameValue = '';
+  imgValue = '';
+  descriptionValue = '';
+  priceValue = 0;
+  discountValue = 0;
+  countValue = 0;  
+  editMode = false;
+  id = -1;
+
+  getAllProducts() {
+    this.productDb.getProducts()
+                  .subscribe((data) => this.products = data)
+  }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  addProduct() {
+    this.productDb.addProduct({
+      name: this.nameValue,
+      img: this.imgValue,
+      description: this.descriptionValue,
+      price: this.priceValue,
+      discount: this.discountValue,
+      count: this.countValue,
+      id: 0
+    })
+    .subscribe ((data) => this.products.push(data));
+    this.nameValue = '';
+    this.descriptionValue = '';
+    this.imgValue = '';
+    this.priceValue = 0;
+    this.discountValue = 0;
+    this.countValue = 0;
+  }
+
+  editProduct (product: ProductModel) {
+    this.editMode = true;
+
+    this.nameValue = product.name;
+    this.imgValue = product.img;
+    this.descriptionValue = product.description;
+    this.priceValue = product.price;
+    this.discountValue = product.discount;
+    this.countValue = product.count;
+    this.id = product.id;
+  }
+
+  saveProduct() {
+    this.productDb.editProduct({
+      name: this.nameValue,
+      img: this.imgValue,
+      description: this.descriptionValue,
+      price: this.priceValue,
+      discount: this.discountValue,
+      count: this.countValue,
+      id: this.id
+    })
+    .subscribe((data) => {
+      this.getAllProducts()
+    });
+
+    this.nameValue = '';
+    this.descriptionValue = '';
+    this.imgValue = '';
+    this.priceValue = 0;
+    this.discountValue = 0;
+    this.countValue = 0;
+    this.editMode = false;
+  }
+
+  cancelProduct() {
+    this.nameValue = '';
+    this.descriptionValue = '';
+    this.imgValue = '';
+    this.priceValue = 0;
+    this.discountValue = 0;
+    this.countValue = 0;
+    this.editMode = false;
+  }
+
+  deleteProduct(id: number) {
+    this.productDb.deleteProduct(id)
+                  .subscribe((data) => {
+                    console.log(data)
+                    this.products = this.products.filter(item => item.id != id)
+                  });
+  }
 }
+
---
+++ src/app/service/products.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private http: HttpClient) { }
+
+  urlDb = 'http://localhost:3000/products';
+
+  getProducts(): Observable<any[]> {
+    return this.http.get<any[]>(this.urlDb);
+  }
+
+  addProduct (product: ProductModel) {
+    return this.http.post<any[]>(this.urlDb, product);
+  }
+
+  editProduct (product: ProductModel) {
+    return this.http.put<any[]>(`${this.urlDb}/${product.id}`, product);
+  }
+
+  deleteProduct (id: number) {
+    return this.http.delete<any[]>(`${this.urlDb}/${id}`);
+  }
+}
+
+export class ProductModel {
+  name: string;
+  img: string;
+  description: string;
+  price: number;
+  discount: number;
+  count: number;
+  id: number;
+}
